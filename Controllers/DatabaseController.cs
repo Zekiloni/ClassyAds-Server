672f5830b81ae815a4ae646d5237d5a051cae88f
Zekiloni <zekirija17@gmail.com>
2023-03-12 21:20:52 +0100

    Add project files.

---
+++ Controllers/DatabaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using WebApplication1.Entities;
+
+public class Database : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        optionsBuilder.UseMySQL(configuration.GetConnectionString("MySqlConnection"));
+    }
+
+    public DbSet<User> Users { get; set; }
+}
+
---
+++ Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Entities
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string emailAdress { get; set; }
+        public DateTime dateOfBirth { get; set; }
+    }
+}
