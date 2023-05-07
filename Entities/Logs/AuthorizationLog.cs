95b96a464f8780148759e8186b59f9b4a6542a48
Zekiloni <zekirija17@gmail.com>
2023-05-07 02:17:52 +0200

    Entities/Logs/AuthorizationLog

---
+++ Entities/Logs/AuthorizationLog.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MyAds.Entities
+{
+    [Table("authorization_logs")]
+    public class AuthorizationLog
+    {
+        [Required]
+        [Column("ip_address")]
+        public string IpAdress { get; set; }
+
+        [Required]
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
