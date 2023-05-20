0cf23bc7dc2f2d6a6a36a97a05498f005db47ec8
Zekiloni <zekirija17@gmail.com>
2023-05-20 22:09:27 +0200

    Notification model

---
+++ Entities/Notification.cs
@@ -0,0 +1,31 @@
+﻿using MyAds.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClassyAdsServer.Entities
+{
+    [Table("notifications")]
+    public class Notification
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        [DefaultValue(false)]
+        [Column("is_read")]
+        public bool IsRead { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public virtual User User { get; set; }
+    }
+}
