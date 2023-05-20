5336cb968edc85b45ed390772d9ff66bc8ff5f03
Zekiloni <zekirija17@gmail.com>
2023-05-20 22:10:56 +0200

    INotificationService

---
+++ Interfaces/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using ClassyAdsServer.Entities;
+
+namespace ClassyAdsServer.Interfaces
+{
+    public interface INotificationService
+    {
+        Task<IEnumerable<Notification>> GetNotificationsByUserId(int userId);
+        Task<Notification> GetNotificationById(int notificationId);
+        Task<int> GetUnreadNotificationCount(int userId);
+        Task CreateNotification(Notification notification);
+        Task MarkNotificationAsRead(int notificationId);
+        Task DeleteNotification(int notificationId);
+    }
+}
