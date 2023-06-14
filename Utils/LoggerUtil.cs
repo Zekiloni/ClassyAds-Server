887b97254fc030376940cfad865d61ac2098e225
Zekiloni <zekirija17@gmail.com>
2023-06-15 00:04:26 +0200

    LoggerUtil

---
+++ Utils/LoggerUtil.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassyAdsServer.Utils
+{
+    public class LoggerUtil
+    {
+        public static void Log(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"[INFO] {message}");
+            Console.ResetColor();
+        }
+
+        public static void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[ERROR] {message}");
+            Console.ResetColor();
+        }
+
+        public static void Warn(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"[WARN] {message}");
+            Console.ResetColor();
+        }
+    }
+}
