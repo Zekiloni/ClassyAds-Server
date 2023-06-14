d588091bfa06ab1cbc25da1f0a579aa173637827
Zekiloni <zekirija17@gmail.com>
2023-06-15 01:46:04 +0200

    MessageService : IMessageService

---
+++ Interfaces/IMessageService.cs
@@ -0,0 +1,19 @@
+﻿using ClassyAdsServer.Entities;
+
+namespace ClassyAdsServer.Interfaces
+{
+    public interface IMessageService
+    {
+        Task<List<Message>> GetAllMessages();
+
+        Task<Message> GetMessageById(int messageId);
+
+        Task<List<Message>> GetMessagesByUserId(int senderId);
+
+        Task CreateMessage(Message message);
+
+        Task UpdateMessage(Message message);
+
+        Task DeleteMessage(Message message);
+    }
+}
---
+++ Services/MessageService.cs
@@ -0,0 +1,46 @@
+﻿using ClassyAdsServer.Entities;
+using ClassyAdsServer.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassyAdsServer.Services
+{
+    public class MessageService : IMessageService
+    {
+        private readonly DatabaseContext _database;
+
+        public MessageService(DatabaseContext dbContext)
+        {
+            _database = dbContext;
+        }
+
+        public Task CreateMessage(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMessage(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Message>> GetAllMessages()
+        {
+            return await _database.Messages.ToListAsync();
+        }
+
+        public Task<Message> GetMessageById(int messageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Message>> GetMessagesByUserId(int senderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateMessage(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
